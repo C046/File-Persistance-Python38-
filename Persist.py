7518b6b53c45036a238b990bde3688779f24f3b1
C046 <79424595+C046@users.noreply.github.com>
2024-02-13 10:24:04 -0800

    Update Persist.py

--- Persist.py
+++ Persist.py
@@ -4,9 +4,28 @@ Created on Sun Feb 11 09:46:56 2024
 
 @author: hadaw
 """
+import threading
+import time
 from SystemFunctions import Create
+
 
 class Persistance(Create):
     def __init__(self, *args, **kwargs):
         super().__init__()
         # And something to work on at work
+        
+    def sleeper(self):
+        if self.file_exists():
+            # If file does exist
+            # do nothing.
+            pass 
+        else:
+            # Generate the file and directory
+            self.generate_filepath()
+            self.create_file()
+        # Sleep for 30 seconds
+        time.sleep(60)
+        
+            
+if __name__ == "__main__":
+    Persistance.sleeper()
