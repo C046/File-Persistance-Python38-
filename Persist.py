e1fc0710164de242ee18bdf3c1f22bcc0d01cc75
C046 <79424595+C046@users.noreply.github.com>
2024-02-13 10:57:52 -0800

    Added drive shuffling

--- Persist.py
+++ Persist.py
@@ -6,13 +6,14 @@ Created on Sun Feb 11 09:46:56 2024
 """
 import threading
 import time
-from SystemFunctions import Create
-
+from File_Persistance_Python38.SystemFunctions import Create
+import numpy as np
 
 class Persistance(Create):
     def __init__(self, *args, **kwargs):
         super().__init__()
-        # And something to work on at work
+        # Assign the sleeper function to a thread to be able to run in the background.
+        self.sleeper_thread = threading.Thread(target=self.sleeper)
         
     def sleeper(self):
         if self.file_exists():
@@ -23,9 +24,16 @@ class Persistance(Create):
             # Generate the file and directory
             self.generate_filepath()
             self.create_file()
+            
         # Sleep for 30 seconds
         time.sleep(60)
+        # Remove the file
+        self._delete()
         
+        # Perform shuffling below
+        drives = np.asanyarray(self.get_drives())
+        drives = np.random.shuffle(drives)
+        print(drives)
             
 if __name__ == "__main__":
-    Persistance.sleeper()
+    Persistance().sleeper()
